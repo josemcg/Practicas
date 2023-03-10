cab6336c408452392f0d8921fb7bac43234bffe3
Ezzy <josemcg1988@gmail.com>
2023-03-10 21:15:54 +0100

    cambio de nombre

---
+++ Practica1/js/clase1.js
@@ -0,0 +1,108 @@
+"use strict";
+//Ejercicio 1
+
+let edad;
+
+let peso;
+
+console.log();
+
+//Ejercicio 2
+
+edad = 20;
+peso = 100;
+
+//Ejercicio 3
+
+let saludo = "Hola Mundo";
+
+console.log(saludo);
+
+//Ejercicio 4
+
+let numeroA = 30;
+let numeroB = 45;
+
+let comodin = numeroA;
+
+numeroA = numeroB;
+
+numeroB = comodin;
+
+//Ejercicio 5
+
+let meGustaElHelado = true;
+
+console.log(meGustaElHelado);
+
+//Ejercicio 6
+
+let suma = 5 + 10;
+let resta = 20 - 5;
+let primerNum = 6;
+let segundoNum = 20;
+let resultado = primerNum * segundoNum;
+let comparacion = 2 > 1;
+console.log(comparacion);
+
+let comparacion2 = 1 > 1;
+
+console.log(compararacion2);
+
+//Ejercicio 7
+
+let respuesta = prompt("Cual es tu edad?");
+console.log("Gracias por tu respuesta");
+console.log("Necesito utilizar variables para procesar tu edad");
+
+//Ejercicio 8
+
+const euros = 7;
+
+dolares = euros * 2;
+
+//Ejercicio 9
+
+const precio = 100;
+
+console.log(precio + (precio * 21) / 100);
+
+//Ejercicio 10
+
+const nombre = "Juan";
+const material = "madera";
+const dimensiones = "diminutas";
+const comentario = "Que sea bonita, pero de una belleza extraña, indómita";
+
+console.log(
+    `${nombre} ha pedido una caja de ${material} con unas dimensiones ${dimensiones}. ${comentario}.`
+);
+
+//Ejercicio 11
+
+let primero = 23;
+let segundo = 15;
+let tercero = 3;
+let cuarto = 2;
+let quinto = 10;
+let resultadoFinal = primero + segundo * (tercero / cuarto) - quinto;
+
+/* 1 */ resultadoFinal -= 5;
+
+/* 2 */ console.log(resultadoFinal--);
+
+let enero = 1;
+let febrero = 2;
+let marzo = 3;
+let abril = 4;
+let mayo = 5;
+let junio = 6;
+let julio = 7;
+let agosto = 8;
+let septiembre = 9;
+let octubre = 10;
+let noviembre = 11;
+let diciembre = 12;
+
+console.log(enero > diciembre);
+console.log(junio < julio);
