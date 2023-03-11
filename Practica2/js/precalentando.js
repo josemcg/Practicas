e4936b76aa1d45158df07fe31471fd45da8b0560
Jose Windows <josemcg1988@gmail.com>
2023-03-11 13:01:23 +0100

    Mitad practica 2

---
+++ Practica2/js/precalentando.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//ejercicio 1
+
+let salir = confirm("¿Vas a salir de tu casa?");
+
+if (salir == true) {
+    alert("Chequea el pronostico del tiempo antes de salir");
+} else {
+    alert("Que bueno que te quedes. Va a llover mucho hoy");
+}
+
+//ejercicio 2
