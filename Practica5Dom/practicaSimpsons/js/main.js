1a892bac44498ed3a1d28894a58e5828468dfefa
Ezzy <josemcg1988@gmail.com>
2023-03-31 03:04:17 +0200

    terminada practica eventos simpsons

--- Practica5Dom/practicaSimpsons/js/main.js
+++ Practica5Dom/practicaSimpsons/js/main.js
@@ -1,25 +1,75 @@
 // completar las variables para que el programa funcione
 
 window.addEventListener("load", function () {
-    //capturar HTML
-    let titulo = document.querySelector("#titulo");
-    titulo.addEventListener("mouseover", (e) => {
-        let nombre = prompt("Como te llamas?");
-        let saludo = document.querySelector("#saludo");
+    // Capturar el título y asignarle el evento correspondiente
+    const titulo = document.querySelector("#titulo");
+    titulo.addEventListener("mouseover", preguntarNombre);
 
-        if (nombre.length > 0) {
-            saludo.innerText = `Bienvenido ${nombre}`;
+    // Función para preguntar el nombre al usuario
+    function preguntarNombre() {
+        const nombre = prompt("¿CÓMO TE LLAMÁS?");
+        const saludo = document.querySelector("#saludo");
+
+        // Validar si el usuario ingresó un nombre o no
+        if (nombre) {
+            saludo.textContent = `BIENVENID@ ${nombre.toUpperCase()}`;
         } else {
-            saludo.innerText = "Bienvenido usuario";
+            saludo.textContent = "BIENVENID@";
         }
-        
+
+        // Convertir el texto del saludo a mayúsculas
+        saludo.style.textTransform = "uppercase";
+
+        // Ocultar el título y mostrar el span
+        titulo.style.display = "none";
+        document.querySelector(".bienvenida span").style.display = "block";
+    }
+
+    // Ocultar la sección bienvenida y mostrar la sección personajes
+    document
+        .querySelector(".bienvenida span")
+        .addEventListener("click", function () {
+            document.querySelector(".bienvenida").style.display = "none";
+            document.querySelector(".personajes").style.display = "flex";
+        });
+
+    // Asignar eventos a cada personaje
+    const bart = document.querySelector("#bart");
+    bart.addEventListener("click", seleccionarPersonaje);
+    const lisa = document.querySelector("#lisa");
+    lisa.addEventListener("dblclick", seleccionarPersonaje);
+    const homero = document.querySelector("#homero");
+    homero.addEventListener("click", seleccionarPersonaje);
+    const marge = document.querySelector("#marge");
+    marge.addEventListener("mouseenter", seleccionarPersonaje);
+    const maggie = document.querySelector("#maggie");
+    maggie.addEventListener("mouseleave", seleccionarPersonaje);
+    const milhouse = document.querySelector("#milhouse");
+    milhouse.addEventListener("click", seleccionarPersonaje);
+    const burns = document.querySelector("#burns");
+    burns.addEventListener("mouseenter", seleccionarPersonaje);
+    const bobPatinio = document.querySelector("#bobPatinio");
+    bobPatinio.addEventListener("dblclick", seleccionarPersonaje);
+    const flanders = document.querySelector("#flanders");
+    flanders.addEventListener("mouseenter", seleccionarPersonaje);
+    const duffman = document.querySelector("#duffman");
+    duffman.addEventListener("dblclick", seleccionarPersonaje);
+    const gorgory = document.querySelector("#gorgory");
+    gorgory.addEventListener("mouseenter", seleccionarPersonaje);
+    const nelson = document.querySelector("#nelson");
+    nelson.addEventListener("click", seleccionarPersonaje);
+
+    // Función para seleccionar/deseleccionar un personaje
+    function seleccionarPersonaje() {
+        this.classList.toggle("personaje-seleccionado");
+    }
+
+    // Mostrar los personajes seleccionados por consola
+    const boton = document.querySelector(".boton");
+    boton.addEventListener("click", () => {
+        const personajesSeleccionados = document.querySelectorAll(
+            ".personaje-seleccionado"
+        );
+        console.log(personajesSeleccionados);
     });
-
-    //  let span =
-
-    /* Capturar pesonajes + Asignarles a cada uno su evento correspondiente, 
-    así como también su funcionalidad */
-
-    /* Asignarle evento a la variable boton */
-    //  let boton =
 });
